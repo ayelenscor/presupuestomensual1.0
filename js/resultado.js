f66784b50e2a2302aa79b20276ce5d6d7bb7ad91
ayelen <scortegagna.ayelen@gmail.com>
2025-07-01 19:19:16 -0300

    changes

--- js/resultado.js
+++ js/resultado.js
@@ -1,13 +1,13 @@
-let presupuesto = localStorage.getItem("presupuesto")
+let presupuesto = localStorage.getItem('presupuesto')
 presupuesto = JSON.parse(presupuesto)
 
 const gastos = ["alquiler", "comida", "servicios"]
 const mostrarResultadoDiv = document.getElementById("mostrarResultado")
 
-detalleGastos.map(nombre => {
-    const gasto = document.createElement("div")
-    gasto.innerHTML= `${nombre}: $${gasto}`
-    gastos.appendChild(gasto)
+gastos.map(nombre => {
+    const detalleGastos = document.createElement("div")
+    detalleGastos.innerHTML= `${nombre}: $${presupuesto.gastos[nombre]}`
+    mostrarResultadoDiv.appendChild(detalleGastos)
   });
 
 
